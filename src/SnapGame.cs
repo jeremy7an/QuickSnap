04c5a454939374b081f268101c9dff71c93273a8
Alibek Maratov <alibekKZ3@gmail.com>
2018-09-17 21:35:42 +0800

    testing
    First commit

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -61,7 +61,7 @@ namespace CardGames
 			{
 				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
 			}
-			SwinGame.DrawCell (SwinGame.BitmapNamed("Cards"),52, 155, 153);
+			SwinGame.DrawCell (SwinGame.BitmapNamed("Cards"),52, 155, 153); //a
 			// Draw the back of the cards... to represent the deck
 
 
