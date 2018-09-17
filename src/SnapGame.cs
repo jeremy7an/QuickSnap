ff1a500dc747b40663ff1b46437d122d28d9b7cb
Jeryxem <43191142+Jeryxem@users.noreply.github.com>
2018-09-17 22:48:24 +0800

    Update SnapGame.cs
    typo error fixed

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,7 +10,7 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-	    SwinGame.LoadFontNamed ("GameFont", "ChunkFive.otf", 24);
+	    SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
         }
 
